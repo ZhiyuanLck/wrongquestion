6bf7115aad085a3760131bfab060e4e03fe1b58c
ZhiyuanLck <L2517996083@163.com>
2020-11-19 22:37:33 +0800

    update

--- update.py
+++ update.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 import pendulum
+import argparse
 
-def rename(name: str) -> None:
+def rename(name: str, args) -> None:
     #  print(path.)
     p = Path('.')
     old = p / f'{name}.pdf'
-    dt = pendulum.tomorrow()
+    dt = pendulum.today().add(days=args.offset[0])
     new = p / f'{name}{dt.isoformat()[0:10]}.pdf'
     if not old.exists():
         print(f'file {old} does not exist!')
@@ -16,6 +17,9 @@ def rename(name: str) -> None:
     old.rename(new)
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = 'update file')
+    parser.add_argument('offset', metavar='N', type=int, nargs=1, help='offset of the date')
+    args = parser.parse_args()
     files = ['math', 'english', 'general']
     for f in files:
-        rename(f)
+        rename(f, args)
